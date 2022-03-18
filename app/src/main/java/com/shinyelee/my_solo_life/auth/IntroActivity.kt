5ccc72d833a8fc4d4cf4348df85c1b9a06d63eb4
shinyelee <shinye0213@naver.com>
2022-03-18 18:57:53 +0900

    Login Page, Join Page (1)

--- app/src/main/java/com/shinyelee/my_solo_life/auth/IntroActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/auth/IntroActivity.kt
@@ -1,12 +1,34 @@
 package com.shinyelee.my_solo_life.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.shinyelee.my_solo_life.R
+import com.shinyelee.my_solo_life.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+//        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        // 로그인 버튼 클릭 -> 로그인 액티비티로 이동
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 회원가입 버튼 클릭 -> 조인 액티비티로 이동
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/shinyelee/my_solo_life/auth/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.shinyelee.my_solo_life.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shinyelee.my_solo_life.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
