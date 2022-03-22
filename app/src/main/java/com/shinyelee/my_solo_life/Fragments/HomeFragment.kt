43be7c51b5010a4247c20197a7fc9942c95022bf
shinyelee <shinye0213@naver.com>
2022-03-22 15:26:42 +0900

    Navigation (5)

--- app/src/main/java/com/shinyelee/my_solo_life/Fragments/HomeFragment.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/Fragments/HomeFragment.kt
@@ -1,11 +1,16 @@
-package com.shinyelee.my_solo_life.Fragments
+package com.shinyelee.my_solo_life.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.shinyelee.my_solo_life.R
+import com.shinyelee.my_solo_life.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,16 +23,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    // binding
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +37,31 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        // binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        // tip으로 이동
+        binding.tipT.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
+            Toast.makeText(context, "팁 클릭됨", Toast.LENGTH_LONG).show()
+        }
+        // talk로 이동
+        binding.talkT.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+            Toast.makeText(context, "토크 클릭됨", Toast.LENGTH_LONG).show()
+        }
+        // bookmark로 이동
+        binding.bookmarkT.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+            Toast.makeText(context, "북마크 클릭됨", Toast.LENGTH_LONG).show()
+        }
+        // store로 이동
+        binding.storeT.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
+            Toast.makeText(context, "스토어 클릭됨", Toast.LENGTH_LONG).show()
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
