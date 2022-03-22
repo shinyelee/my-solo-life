1f74bf29a2d664ff72bccbc5e578a94f911ae73f
shinyelee <shinye0213@naver.com>
2022-03-23 00:20:10 +0900

    RecyclerView (3) Adapter & Activity

--- app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
@@ -2,6 +2,9 @@ package com.shinyelee.my_solo_life.contentsList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.shinyelee.my_solo_life.R
 
 class ContentsListActivity : AppCompatActivity() {
@@ -10,6 +13,18 @@ class ContentsListActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contents_list)
+
+        val rv : RecyclerView = findViewById(R.id.rv)
+
+        val items = ArrayList<String>()
+        items.add("a")
+        items.add("b")
+        items.add("c")
+
+        val rvAdapter = ContentsRVAdapter(items)
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = GridLayoutManager(this, 2)
 
     }
 
--- app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsRVAdapter.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsRVAdapter.kt
@@ -1,6 +1,37 @@
 package com.shinyelee.my_solo_life.contentsList
 
-class ContentsRVAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shinyelee.my_solo_life.R
 
+class ContentsRVAdapter(val items : ArrayList<String>) : RecyclerView.Adapter<ContentsRVAdapter.Viewholder>() {
+
+    // item 하나 가져옴
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ContentsRVAdapter.Viewholder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.contents_rv_item, parent, false)
+        return Viewholder(v)
+    }
+
+    // item 내용
+    override fun onBindViewHolder(holder: ContentsRVAdapter.Viewholder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+    // 전체 item 개수
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // item에 데이터 넣어줌
+    inner class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(item : String) {
+
+        }
+    }
 
 }
