fb25b596f7582ee6e4bc03e9b572b2cdf31a768c
shinyelee <shinye0213@naver.com>
2022-03-22 21:49:38 +0900

    RecyclerView (1) ContentsList layout

---
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
@@ -0,0 +1,16 @@
+package com.shinyelee.my_solo_life.contentsList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shinyelee.my_solo_life.R
+
+class ContentsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contents_list)
+
+    }
+
+}
