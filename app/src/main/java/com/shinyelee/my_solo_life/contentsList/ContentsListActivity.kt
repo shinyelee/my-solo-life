2163037cdf774a511560cfd9018bee4184424cba
shinyelee <shinye0213@naver.com>
2022-03-24 23:42:08 +0900

    data from Firebase (1)

--- app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
@@ -3,11 +3,15 @@ package com.shinyelee.my_solo_life.contentsList
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.shinyelee.my_solo_life.R
@@ -19,9 +23,50 @@ class ContentsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contents_list)
 
+        val items = ArrayList<ContentsModel>()
+
         // Write a message to the database
         val database = Firebase.database
         val myRef = database.getReference("contents")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for(dataModel in dataSnapshot.children) {
+                    Log.d("ContentsListActivity", dataModel.toString())
+                    val item = dataModel.getValue(ContentsModel::class.java)
+                    items.add(item!!)
+                }
+                Log.d("ContentsListActivity", items.toString())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("ContentsListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        myRef.addValueEventListener(postListener)
+
+        val rv : RecyclerView = findViewById(R.id.rv)
+
+        val rvAdapter = ContentsRVAdapter(baseContext, items)
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = GridLayoutManager(this, 2)
+
+        // item click
+        rvAdapter.itemClick = object : ContentsRVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                Toast.makeText(baseContext, items[position].title, Toast.LENGTH_LONG).show()
+                val intent = Intent(this@ContentsListActivity, ContentsShowActivity::class.java)
+                intent.putExtra("url", items[position].webUrl)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+}
+
 //        myRef.push().setValue(
 //            ContentsModel("title1", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png", "https://philosopher-chan.tistory.com/1235")
 //        )
@@ -35,9 +80,6 @@ class ContentsListActivity : AppCompatActivity() {
 //            ContentsModel("title4", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOYyBM%2Fbtq67Or43WW%2F17lZ3tKajnNwGPSCLtfnE1%2Fimg.png", "https://philosopher-chan.tistory.com/1238")
 //        )
 
-        val rv : RecyclerView = findViewById(R.id.rv)
-
-        val items = ArrayList<ContentsModel>()
 //        items.add(ContentsModel("title1", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png", "https://philosopher-chan.tistory.com/1235"))
 //        items.add(ContentsModel("title2", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png", "https://philosopher-chan.tistory.com/1236"))
 //        items.add(ContentsModel("title3", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png", "https://philosopher-chan.tistory.com/1237"))
@@ -53,23 +95,3 @@ class ContentsListActivity : AppCompatActivity() {
 //        items.add(ContentsModel("title13", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbdIKDG%2Fbtq64M96JFa%2FKcJiYgKuwKuP3fIyviXm90%2Fimg.png", "https://philosopher-chan.tistory.com/1247"))
 //        items.add(ContentsModel("title14", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFtY3t%2Fbtq65q6P4Zr%2FWe64GM8KzHAlGE3xQ2nDjk%2Fimg.png", "https://philosopher-chan.tistory.com/1248"))
 //        items.add(ContentsModel("title15", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FOtaMq%2Fbtq67OMpk4W%2FH1cd0mda3n2wNWgVL9Dqy0%2Fimg.png", "https://philosopher-chan.tistory.com/1249"))
-
-        val rvAdapter = ContentsRVAdapter(baseContext, items)
-        rv.adapter = rvAdapter
-
-        rv.layoutManager = GridLayoutManager(this, 2)
-
-        // item click
-        rvAdapter.itemClick = object : ContentsRVAdapter.ItemClick {
-            override fun onClick(view: View, position: Int) {
-                Toast.makeText(baseContext, items[position].title, Toast.LENGTH_LONG).show()
-                val intent = Intent(this@ContentsListActivity, ContentsShowActivity::class.java)
-                intent.putExtra("url", items[position].webUrl)
-                startActivity(intent)
-            }
-
-        }
-
-    }
-
-}
