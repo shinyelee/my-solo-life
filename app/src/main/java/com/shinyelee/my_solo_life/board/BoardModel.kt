ee17c4a1d907571fe717bf098c3c5fdd8d88af5f
shinyelee <shinye0213@naver.com>
2022-03-28 20:47:28 +0900

    talk (4) add data

---
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardModel.kt
@@ -0,0 +1,8 @@
+package com.shinyelee.my_solo_life.board
+
+class BoardModel (
+    val title : String?,
+    val contents : String?,
+    val uid : String?,
+    val time : String
+)
--- app/src/main/java/com/shinyelee/my_solo_life/board/BoardWriteActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardWriteActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.shinyelee.my_solo_life.R
+import com.shinyelee.my_solo_life.contentsList.BookmarkModel
 import com.shinyelee.my_solo_life.databinding.ActivityBoardWriteBinding
+import com.shinyelee.my_solo_life.utils.FBAuth
+import com.shinyelee.my_solo_life.utils.FBRef
 
 class BoardWriteActivity : AppCompatActivity() {
 
@@ -21,10 +24,17 @@ class BoardWriteActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_write)
 
         binding.writeBtn.setOnClickListener {
+
             val title = binding.titleArea.text.toString()
-            val content = binding.contentsArea.text.toString()
+            val contents = binding.contentsArea.text.toString()
+
             Log.d(TAG, title)
-            Log.d(TAG, content)
+            Log.d(TAG, contents)
+
+            FBRef.boardRef
+                .push()
+                .setValue(BoardModel(title,contents,"uid","time"))
+
         }
     }
 }
