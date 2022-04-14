6cc790b1d2bd8d0d76dd5e1c8de5bd3a9d4b103a
shinyelee <shinye0213@naver.com>
2022-04-14 21:23:18 +0900

    read contents page layout

--- app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
@@ -3,20 +3,29 @@ package com.shinyelee.my_solo_life.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.shinyelee.my_solo_life.R
+import com.shinyelee.my_solo_life.databinding.ActivityBoardInsideBinding
 
 class BoardInsideActivity : AppCompatActivity() {
 
     private val TAG = BoardInsideActivity::class.java.simpleName
 
+    private lateinit var binding : ActivityBoardInsideBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_inside)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
 
         val title = intent.getStringExtra("title").toString()
         val contents = intent.getStringExtra("contents").toString()
         val time = intent.getStringExtra("time").toString()
+
+        binding.titleArea.text = title
+        binding.textArea.text = contents
+        binding.timeArea.text = time
 
         Log.d(TAG, title)
         Log.d(TAG, contents)
