bfa4f151a614065c70bc806d533c24553e4873d7
shinyelee <shinye0213@naver.com>
2022-04-14 17:45:49 +0900

    read contents (2)

--- app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
@@ -2,14 +2,25 @@ package com.shinyelee.my_solo_life.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.shinyelee.my_solo_life.R
 
 class BoardInsideActivity : AppCompatActivity() {
+
+    private val TAG = BoardInsideActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_inside)
+
+        val title = intent.getStringExtra("title").toString()
+        val contents = intent.getStringExtra("contents").toString()
+        val time = intent.getStringExtra("time").toString()
+
+        Log.d(TAG, title)
+        Log.d(TAG, contents)
+        Log.d(TAG, time)
 
     }
 
