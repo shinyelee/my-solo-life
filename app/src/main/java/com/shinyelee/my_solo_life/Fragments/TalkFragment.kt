e25cf2426138167b5dbd4e8280c312b798a33c22
shinyelee <shinye0213@naver.com>
2022-04-15 22:35:56 +0900

    load post info (1)

--- app/src/main/java/com/shinyelee/my_solo_life/Fragments/TalkFragment.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/Fragments/TalkFragment.kt
@@ -28,7 +28,7 @@ class TalkFragment : Fragment() {
     private lateinit var binding : FragmentTalkBinding
 
     private val boardDataList = mutableListOf<BoardModel>()
-
+    private val boardKeyList = mutableListOf<String>()
     private val TAG = TalkFragment::class.java.simpleName
 
     private lateinit var boardRVAdapter: BoardListLVAdapter
@@ -50,21 +50,24 @@ class TalkFragment : Fragment() {
 
         binding.boardListView.setOnItemClickListener { parent, view, position, id ->
 
+            // 첫 번째 방법 //
+            // listView에 있는 데이터 title, contents, time 다 다른 액티비티로 전달해서 만드는 방법
+//
+//            val intent = Intent(context, BoardInsideActivity::class.java)
+//
+//            intent.putExtra("title", boardDataList[position].title)
+//            intent.putExtra("contents", boardDataList[position].contents)
+//            intent.putExtra("time", boardDataList[position].time)
+//
+//            startActivity(intent)
+
+            // 두 번째 방법 //
+            // Firebase 자체 데이터 id(uid 아님)를 기반으로 다시 데이터를 받아오는 방법
             val intent = Intent(context, BoardInsideActivity::class.java)
-
-            intent.putExtra("title", boardDataList[position].title)
-            intent.putExtra("contents", boardDataList[position].contents)
-            intent.putExtra("time", boardDataList[position].time)
-
+            intent.putExtra("key", boardKeyList[position])
             startActivity(intent)
 
         }
-
-        // 첫 번째 방법 //
-        // listView에 있는 데이터 title, contents, time 다 다른 액티비티로 전달해서 만드는 방법
-
-        // 두 번째 방법 //
-        // Firebase 자체 데이터 id(uid 아님)를 기반으로 다시 데이터를 받아오는 방법
 
         // talk -> write
         binding.writeBtn.setOnClickListener {
@@ -105,10 +108,13 @@ class TalkFragment : Fragment() {
 
                 for(dataModel in dataSnapshot.children) {
                     Log.d(TAG, dataModel.toString())
+//                    dataModel.key
 
                     val item = dataModel.getValue(BoardModel::class.java)
                     boardDataList.add(item!!)
+                    boardKeyList.add(dataModel.key.toString())
                 }
+                boardDataList.reverse()
                 boardRVAdapter.notifyDataSetChanged()
                 Log.d(TAG, boardDataList.toString())
 
--- app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
@@ -3,6 +3,7 @@ package com.shinyelee.my_solo_life.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.shinyelee.my_solo_life.R
 import com.shinyelee.my_solo_life.databinding.ActivityBoardInsideBinding
@@ -19,17 +20,22 @@ class BoardInsideActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
 
-        val title = intent.getStringExtra("title").toString()
-        val contents = intent.getStringExtra("contents").toString()
-        val time = intent.getStringExtra("time").toString()
-
-        binding.titleArea.text = title
-        binding.textArea.text = contents
-        binding.timeArea.text = time
+        // 첫 번째 방법
+//        val title = intent.getStringExtra("title").toString()
+//        val contents = intent.getStringExtra("contents").toString()
+//        val time = intent.getStringExtra("time").toString()
+//
+//        binding.titleArea.text = title
+//        binding.textArea.text = contents
+//        binding.timeArea.text = time
 
 //        Log.d(TAG, title)
 //        Log.d(TAG, contents)
 //        Log.d(TAG, time)
+
+        // 두 번째 방법
+        val key = intent.getStringExtra("key")
+        Toast.makeText(this, key, Toast.LENGTH_LONG).show()
 
     }
 
