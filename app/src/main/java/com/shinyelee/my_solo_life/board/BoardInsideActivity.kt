9054d5f2cea03320cb7da6ba6b762522f685281c
shinyelee <shinye0213@naver.com>
2022-04-16 19:14:58 +0900

    image download (1)

--- app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardInsideActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.shinyelee.my_solo_life.R
 import com.shinyelee.my_solo_life.databinding.ActivityBoardInsideBinding
 import com.shinyelee.my_solo_life.utils.FBRef
@@ -33,14 +36,26 @@ class BoardInsideActivity : AppCompatActivity() {
 //        binding.textArea.text = contents
 //        binding.timeArea.text = time
 
-//        Log.d(TAG, title)
-//        Log.d(TAG, contents)
-//        Log.d(TAG, time)
-
         // 두 번째 방법
         val key = intent.getStringExtra("key")
-//        Toast.makeText(this, key, Toast.LENGTH_LONG).show()
         getBoardData(key.toString())
+        getImageData(key.toString())
+
+    }
+
+    private fun getImageData(key : String) {
+
+        // Reference to an image file in Cloud Storage
+        val storageReference = Firebase.storage.reference.child(key + ".png")
+
+        // ImageView in your Activity
+        val imageView = binding.getImageArea
+
+        // Download directly from StorageReference using Glide
+        // (See MyAppGlideModule for Loader registration)
+        Glide.with(this /* context */)
+            .load(storageReference)
+            .into(imageView)
 
     }
 
