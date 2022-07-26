79839e4836c1304f0b5ec5de7b0c6d02e851f94f
shinyelee <shinye0213@naver.com>
2022-07-26 20:04:09 +0900

    comments (intro, join, login, splash, gradle)

--- app/src/main/java/com/shinyelee/my_solo_life/auth/IntroActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/auth/IntroActivity.kt
@@ -14,22 +14,28 @@ class IntroActivity : AppCompatActivity() {
 
     private val TAG = "IntroActivity"
 
-    // firebase
+    // 파이어베이스 인스턴스 선언
     private lateinit var auth: FirebaseAuth
 
-    // viewBinding
+    // (전역변수) 바인딩 객체 선언
     private var vBinding : ActivityIntroBinding? = null
+
+    // 매번 null 확인 귀찮음 -> 바인딩 변수 재선언
     private val binding get() = vBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        // firebase
-        auth = Firebase.auth
-
         super.onCreate(savedInstanceState)
 
-        // viewBinding
+        // 파이어베이스 인스턴스 초기화
+        auth = Firebase.auth
+
+        // 자동 생성된 뷰바인딩 클래스에서의 inflate 메서드 활용
+        // -> 액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
         vBinding = ActivityIntroBinding.inflate(layoutInflater)
+
+        // getRoot 메서드로 레이아웃 내부 최상위에 있는 뷰의 인스턴스 활용
+        // -> 생성된 뷰를 액티비티에 표시
         setContentView(binding.root)
 
         // 로그인 버튼 클릭하면
@@ -53,24 +59,24 @@ class IntroActivity : AppCompatActivity() {
         // 비회원 버튼 클릭하면
         binding.guestBtn.setOnClickListener {
 
-            // 비회원으로 로그인
+            // 익명으로 로그인
             auth.signInAnonymously()
                 .addOnCompleteListener(this) { task ->
 
+                    // 성공하면
                     if (task.isSuccessful) {
-
-                        // 로그인 성공
-                        Toast.makeText(this, "비회원으로 로그인되었습니다", Toast.LENGTH_LONG).show()
 
                         // 메인 액티비티로 이동
                         val intent = Intent(this, MainActivity::class.java)
-                        // 인트로 액티비티 종료
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+                        // 기존 스택을 비움 -> 뒤로가기 했을 때 로그인 화면으로 되돌아가는 것 방지
+                        finishAffinity()
                         startActivity(intent)
 
+                    // 실패하면
                     } else {
 
-                        // 로그인 실패
+                        // 토스트 메시지 띄움
                         Toast.makeText(this, "비회원 로그인에 실패했습니다", Toast.LENGTH_LONG).show()
 
                     }
@@ -81,9 +87,14 @@ class IntroActivity : AppCompatActivity() {
 
     }
 
+    // 액티비티 파괴시
     override fun onDestroy() {
+
+        // 바인딩 클래스 인스턴스 참조를 정리
+        // -> 메모리 효율이 좋아짐
         vBinding = null
         super.onDestroy()
+
     }
 
 }
--- app/src/main/java/com/shinyelee/my_solo_life/auth/LoginActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/auth/LoginActivity.kt
@@ -15,22 +15,28 @@ class LoginActivity : AppCompatActivity() {
 
     private val TAG = "LoginActivity"
 
-    // firebase
+    // 파이어베이스 인스턴스 선언
     private lateinit var auth: FirebaseAuth
 
-    // viewBinding
+    // (전역변수) 바인딩 객체 선언
     private var vBinding : ActivityLoginBinding? = null
+
+    // 매번 null 확인 귀찮음 -> 바인딩 변수 재선언
     private val binding get() = vBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-        // firebase
+        // 파이어베이스 인스턴스 초기화
         auth = Firebase.auth
 
-        // viewBinding
+        // 자동 생성된 뷰바인딩 클래스에서의 inflate 메서드 활용
+        // -> 액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
         vBinding = ActivityLoginBinding.inflate(layoutInflater)
+
+        // getRoot 메서드로 레이아웃 내부 최상위에 있는 뷰의 인스턴스 활용
+        // -> 생성된 뷰를 액티비티에 표시
         setContentView(binding.root)
 
         // 로그인 버튼 클릭하면
@@ -41,7 +47,7 @@ class LoginActivity : AppCompatActivity() {
             var pwCheck = true
             var allCheck = emailCheck and pwCheck
 
-            // 이메일주소, 비밀번호
+            // 이메일, 비밀번호
             val emailTxt = binding.email.text.toString()
             val pwTxt = binding.pw.text.toString()
 
@@ -51,10 +57,10 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "입력란을 모두 작성하세요", Toast.LENGTH_SHORT).show()
             }
 
-            // 이메일주소 정규식
+            // 이메일 정규식
             val emailPattern = Patterns.EMAIL_ADDRESS
 
-            // 이메일주소 검사
+            // 이메일 검사
             if(emailTxt.isEmpty()) {
                 emailCheck = false
                 binding.emailArea.error = "이메일주소를 입력하세요"
@@ -81,24 +87,27 @@ class LoginActivity : AppCompatActivity() {
                 binding.pwArea.error = null
             }
 
-            // 로그인 조건 모두 만족하면
+            // 로그인 조건을 모두 만족하면
             if(allCheck) {
 
-                // 로그인 실행
+                // 로그인
                 auth.signInWithEmailAndPassword(emailTxt, pwTxt)
                     .addOnCompleteListener(this) { task ->
 
-                        // 로그인 성공
+                        // 성공하면
                         if (task.isSuccessful) {
 
-                            // 스택에 쌓인 액티비티 종료하고 메인 액티비티로 이동
+                            // 메인 액티비티로 이동
                             val intent = Intent(this, MainActivity::class.java)
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+                            // 기존 스택을 비움 -> 뒤로가기 했을 때 로그인 화면으로 되돌아가는 것 방지
+                            finishAffinity()
                             startActivity(intent)
 
-                        // 로그인 실패
+                        // 실패하면
                         } else {
 
+                            // 메시지 띄움
                             Toast.makeText(this, "로그인에 실패했습니다", Toast.LENGTH_LONG).show()
 
                         }
@@ -115,9 +124,14 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    // 액티비티 파괴시
     override fun onDestroy() {
+
+        // 바인딩 클래스 인스턴스 참조를 정리
+        // -> 메모리 효율이 좋아짐
         vBinding = null
         super.onDestroy()
+
     }
 
 }
