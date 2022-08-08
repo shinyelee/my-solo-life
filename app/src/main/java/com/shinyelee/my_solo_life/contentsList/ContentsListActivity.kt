4e4ff7d58c95a0c81ff5bc0d68946ddd09f0e15b
shinyelee <shinye0213@naver.com>
2022-08-08 14:58:46 +0900

    Update ContentsListActivity, activity_contents_list(back button)

--- app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
@@ -27,16 +27,30 @@ class ContentsListActivity : AppCompatActivity() {
         // -> 생성된 뷰를 액티비티에 표시
         setContentView(binding.root)
 
+        // 뒤로가기 버튼 클릭하면
+        binding.backBtn.setOnClickListener {
+
+            // 콘텐츠리스트 액티비티 종료
+            finish()
+
+        }
+
         // 리사이클러 뷰 어댑터 연결
 //        val rv : RecyclerView = findViewById(R.id.rv)
         val rv : RecyclerView = binding.rv
 
         // 리사이클러 뷰에 테스트 데이터 넣음
         val items = ArrayList<ContentsModel>()
-        items.add(ContentsModel("imageUrl1", "title1"))
-        items.add(ContentsModel("imageUrl2", "title2"))
-        items.add(ContentsModel("imageUrl3", "title3"))
-        items.add(ContentsModel("imageUrl4", "title4"))
+        items.add(ContentsModel("android.databinding.tool.processing.ScopedException: [databinding]", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcjbcPT%2FbtrHN4eVbuv%2FsOtXJrQU89FbDsbxG67DNK%2Fimg.png"))
+        items.add(ContentsModel("Cannot access '어쩌고' which is a supertype of '패키지명.databinding.액티비티명Binding'.", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbSdctw%2FbtrHI2PS3ZL%2FdPipHXtospsMS6FASG8FG1%2Fimg.png"))
+        items.add(ContentsModel("E/AndroidRuntime: FATAL EXCEPTION: DefaultDispatcher-worker-1 (2)", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcrqopJ%2FbtrFpbueRw7%2F4DFoMUj5QjYOhO8ReixI6K%2Fimg.png"))
+        items.add(ContentsModel("E/AndroidRuntime: FATAL EXCEPTION: DefaultDispatcher-worker-1 (1)", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FVEkQJ%2FbtrFaNGvPZG%2FHfnKE2LwuKpklaV1kZOcDk%2Fimg.png"))
+        items.add(ContentsModel("패키지.MainActivity is not an Activity subclass or alias", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FVv3Jp%2FbtrC3E7WW01%2FJeS6AntHwhXXt5DtBRpWi0%2Fimg.png"))
+        items.add(ContentsModel("System UI Bar Error", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fk9q0o%2FbtrBq7JmNyk%2F0i6Thp20dzueTh2kRCRlKk%2Fimg.png"))
+        items.add(ContentsModel("E/ClipboardService: Denying clipboard access to com.android.", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLwxRZ%2FbtrAM2nFkOb%2FPBBtZGc5TUdeqSlCSyoK70%2Fimg.png"))
+        items.add(ContentsModel("Kotlin: [Internal Error] java.rmi.UnexpectedException: unexpected exception; nested exception is:", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdnOUIn%2FbtryPTgoVCB%2FERI1WJaXHK7CEjiItyDxyK%2Fimg.png"))
+        items.add(ContentsModel("Unrecognized Android Studio (or Android Support plugin for IntelliJ IDEA) version '202.7660.26.42.7351085',", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbjqRTI%2Fbtrti4sRRpa%2FGv9yCVzLCxjpOFB8Qtg5tK%2Fimg.png"))
+        items.add(ContentsModel("java.lang.RuntimeException: Unable to start activity ComponentInfo{패키지명/패키지명.MainActivity}:", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOnksa%2Fbtrs660xdJT%2FJ3xIZIhrr5G4LQQzq36I11%2Fimg.png"))
 
         // 리사이클러 뷰 어댑터 연결
         val rvAdapter = ContentsRVAdapter(items)
--- app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsModel.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsModel.kt
@@ -1,8 +1,12 @@
 package com.shinyelee.my_solo_life.contentsList
 
+// 블로그 게시글 데이터 모델
 class ContentsModel (
 
+    // 블로그 게시글 제목
     var title : String = "",
+
+    // 블로그 게시글 이미지 URL
     var imageUrl : String = ""
 
 )
