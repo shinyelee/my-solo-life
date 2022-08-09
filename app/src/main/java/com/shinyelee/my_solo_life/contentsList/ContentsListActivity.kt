87b197441fc388be487476c4d85e2d13f6ab60ef
shinyelee <shinye0213@naver.com>
2022-08-09 21:07:57 +0900

    Update ContentsListActivity(firebase data load) (4)

--- app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/contentsList/ContentsListActivity.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -22,6 +23,10 @@ class ContentsListActivity : AppCompatActivity() {
 
     // 매번 null 확인 귀찮음 -> 바인딩 변수 재선언
     private val binding get() = vBinding!!
+
+    // lateinit var
+    // -> 우선 데이터 타입(파이어베이스 DB)만 정해놓고 값은 나중에 넣음
+    lateinit var myRef : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -47,146 +52,66 @@ class ContentsListActivity : AppCompatActivity() {
         // 카테고리
         val category = intent.getStringExtra("category")
 
+        // 카테고리가 kotlin_syntax면
         if(category == "kotlin_syntax") {
 
-            // kotlin_syntax 하위에 데이터 넣음
-            val myRef = database.getReference("kotlin_syntax")
+            // 파이어베이스의 kotlin_syntax 하위 데이터 가져옴
+            myRef = database.getReference("kotlin_syntax")
 
-
-            // 데이터베이스에서 게시물의 세부정보를 검색
-            val postListener = object : ValueEventListener {
-
-                // 데이터 스냅샷
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                    // for문으로 출력
-                    for(dataModel in dataSnapshot.children) {
-
-                        // 데이터모델 형태로 받아옴
-                        val item = dataModel.getValue(ContentsModel::class.java)
-
-                        // 데이터 리스트에 아이템 넣어줌
-                        items.add(item!!)
-
-                    }
-
-                    // 아이템의 변경사항을 어댑터에 알려줌
-                    rvAdapter.notifyDataSetChanged()
-
-                    // items에 들어간 데이터 확인
-                    Log.d("ContentsListActivity", items.toString())
-
-                }
-
-                // 오류 나면
-                override fun onCancelled(databaseError: DatabaseError) {
-
-                    // 로그
-                    Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
-
-                }
-
-            }
-
-            // 파이어베이스 내 데이터 변화(추가)를 알려줌
-            myRef.addValueEventListener(postListener)
-
-
+        // 카테고리가 kotlin_syntax면
         } else if(category == "error_warning") {
 
-            // error_warning 하위에 데이터 넣음
-            val myRef = database.getReference("error_warning")
+            // 파이어베이스의 kotlin_syntax 하위 데이터 가져옴
+            myRef = database.getReference("error_warning")
 
-
-            // 데이터베이스에서 게시물의 세부정보를 검색
-            val postListener = object : ValueEventListener {
-
-                // 데이터 스냅샷
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                    // for문으로 출력
-                    for(dataModel in dataSnapshot.children) {
-
-                        // 데이터모델 형태로 받아옴
-                        val item = dataModel.getValue(ContentsModel::class.java)
-
-                        // 데이터 리스트에 아이템 넣어줌
-                        items.add(item!!)
-
-                    }
-
-                    // 아이템의 변경사항을 어댑터에 알려줌
-                    rvAdapter.notifyDataSetChanged()
-
-                    // items에 들어간 데이터 확인
-                    Log.d("ContentsListActivity", items.toString())
-
-                }
-
-                // 오류 나면
-                override fun onCancelled(databaseError: DatabaseError) {
-
-                    // 로그
-                    Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
-
-                }
-
-            }
-
-            // 파이어베이스 내 데이터 변화(추가)를 알려줌
-            myRef.addValueEventListener(postListener)
-
-
+        // 카테고리가 kotlin_syntax면
         } else {
 
-
-            // contents 하위에 데이터 넣음
-            val myRef = database.getReference("contents")
-
-            // 데이터베이스에서 게시물의 세부정보를 검색
-            val postListener = object : ValueEventListener {
-
-                // 데이터 스냅샷
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                    // for문으로 출력
-                    for(dataModel in dataSnapshot.children) {
-
-                        // 데이터모델 형태로 받아옴
-                        val item = dataModel.getValue(ContentsModel::class.java)
-
-                        // 데이터 리스트에 아이템 넣어줌
-                        items.add(item!!)
-
-                    }
-
-                    // 아이템의 변경사항을 어댑터에 알려줌
-                    rvAdapter.notifyDataSetChanged()
-
-                    // items에 들어간 데이터 확인
-                    Log.d("ContentsListActivity", items.toString())
-
-                }
-
-                // 오류 나면
-                override fun onCancelled(databaseError: DatabaseError) {
-
-                    // 로그
-                    Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
-
-                }
-
-            }
-
-            // 파이어베이스 내 데이터 변화(추가)를 알려줌
-            myRef.addValueEventListener(postListener)
-
+            // 파이어베이스의 kotlin_syntax 하위 데이터 가져옴
+            myRef = database.getReference("contents")
 
         }
 
+        // 데이터베이스에서 게시물의 세부정보를 검색
+        val postListener = object : ValueEventListener {
+
+            // 데이터 스냅샷
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                // for문으로 출력
+                for(dataModel in dataSnapshot.children) {
+
+                    // 데이터모델 형태로 받아옴
+                    val item = dataModel.getValue(ContentsModel::class.java)
+
+                    // 데이터 리스트에 아이템 넣어줌
+                    items.add(item!!)
+
+                }
+
+                // 아이템의 변경사항을 어댑터에 알려줌
+                rvAdapter.notifyDataSetChanged()
+
+                // items에 들어간 데이터 확인
+                Log.d("ContentsListActivity", items.toString())
+
+            }
+
+            // 오류 나면
+            override fun onCancelled(databaseError: DatabaseError) {
+
+                // 로그
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+
+            }
+
+        }
+
+        // 파이어베이스 내 데이터 변화(추가)를 알려줌
+        myRef.addValueEventListener(postListener)
+
         // 리사이클러뷰 어댑터 연결
         val rv : RecyclerView = binding.rv
-
         rv.adapter = rvAdapter
 
         // 그리드 레이아웃 매니저 -> 아이템을 격자 형태로 배치(2열)
