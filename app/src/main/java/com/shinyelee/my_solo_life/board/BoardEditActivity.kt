35858bf9271782339a74c76bd9f4d697c1a7fe6e
shinyelee <shinye0213@naver.com>
2022-08-19 14:34:05 +0900

    Create BoardEditActivity, Update BoardReadActivity(edit page)

--- app/src/main/java/com/shinyelee/my_solo_life/board/BoardEditActivity.kt
+++ app/src/main/java/com/shinyelee/my_solo_life/board/BoardEditActivity.kt
@@ -1,28 +1,22 @@
 package com.shinyelee.my_solo_life.board
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import com.shinyelee.my_solo_life.R
-import com.shinyelee.my_solo_life.databinding.ActivityBoardReadBinding
+import com.shinyelee.my_solo_life.databinding.ActivityBoardEditBinding
 import com.shinyelee.my_solo_life.utils.FBRef
 
-class BoardReadActivity : AppCompatActivity() {
+class BoardEditActivity : AppCompatActivity() {
 
     // (전역변수) 바인딩 객체 선언
-    private var vBinding : ActivityBoardReadBinding? = null
+    private var vBinding : ActivityBoardEditBinding? = null
 
     // 매번 null 확인 귀찮음 -> 바인딩 변수 재선언
     private val binding get() = vBinding!!
@@ -31,7 +25,7 @@ class BoardReadActivity : AppCompatActivity() {
     private lateinit var key: String
 
     // 태그
-    private val TAG = BoardReadActivity::class.java.simpleName
+    private val TAG = BoardEditActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -39,16 +33,11 @@ class BoardReadActivity : AppCompatActivity() {
 
         // 자동 생성된 뷰바인딩 클래스에서의 inflate 메서드 활용
         // -> 액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
-        vBinding = ActivityBoardReadBinding.inflate(layoutInflater)
+        vBinding = ActivityBoardEditBinding.inflate(layoutInflater)
 
         // getRoot 메서드로 레이아웃 내부 최상위에 있는 뷰의 인스턴스 활용
         // -> 생성된 뷰를 액티비티에 표시
         setContentView(binding.root)
-
-        // 게시글 설정 버튼 -> 대화상자 뜸
-        binding.boardSettingBtn.setOnClickListener {
-            showDialog()
-        }
 
         // 글읽기 프래그먼트에서 게시글의 키 값을 받아옴
         key = intent.getStringExtra("key").toString()
@@ -56,49 +45,6 @@ class BoardReadActivity : AppCompatActivity() {
         // 키 값을 바탕으로 게시글 하나의 정보를 가져옴
         getPostData(key)
         getImageData(key)
-
-    }
-
-    // 내가 쓴 글 수정/삭제 대화상자
-    private fun showDialog() {
-
-        // custom_dialog를 뷰 객체로 반환
-        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
-
-        // 대화상자 생성
-        val mBuilder = AlertDialog.Builder(this)
-            .setView(mDialogView)
-
-        // 대화상자 띄움
-        val alertDialog = mBuilder.show()
-
-        // 수정 버튼
-        alertDialog.findViewById<ConstraintLayout>(R.id.edit)?.setOnClickListener {
-
-            // 명시적 인텐트 -> 다른 액티비티 호출
-            val intent = Intent(this, BoardEditActivity::class.java)
-
-            // 키 값을 바탕으로 게시글 받아옴
-            intent.putExtra("key", key)
-
-            // 글수정 액티비티 시작
-            startActivity(intent)
-
-        }
-
-        // 삭제 버튼
-        alertDialog.findViewById<ConstraintLayout>(R.id.delete)?.setOnClickListener {
-
-            // -> 게시글 삭제
-            FBRef.boardRef.child(key).removeValue()
-
-            // 글읽기 액티비티 종료
-            finish()
-
-            // 삭제 확인 메시지
-            Toast.makeText(this, "게시글이 삭제되었습니다", Toast.LENGTH_SHORT).show()
-
-        }
 
     }
 
@@ -140,16 +86,17 @@ class BoardReadActivity : AppCompatActivity() {
                     // 데이터 스냅샷 내 데이터모델 형식으로 저장된 아이템(=게시글)
                     val item = dataSnapshot.getValue(BoardModel::class.java)
 
-                    // 제목, 시간, 본문 해당 영역에 넣음
-                    binding.titleArea.text = item!!.title
-                    binding.timeArea.text = item.time
-                    binding.mainArea.text = item.main
+                    // 제목, 본문 해당 영역에 넣음(시간은 수정하지 않음)
+                    binding.titleArea.setText(item?.title)
+                    binding.mainArea.setText(item?.main)
+                    // textView -> .text
+                    // editText -> .setText(집어넣을 데이터)
 
-                // 오류 나면
+                    // 오류 나면
                 } catch (e: Exception) {
 
                     // 로그
-                    Log.d(TAG, "getPostData 확인")
+                    Log.e(TAG, "getPostData 확인")
 
                 }
 
